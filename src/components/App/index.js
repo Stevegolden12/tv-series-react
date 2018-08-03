ab5543735a62b987c8ec35273b6140535723fa7f
Steve Golden <sgolden2525@outlook.com>
2018-08-03 14:53:58 -0600

    Started project
    Added header, title
    Added fetch polyfill to get movie API.
    Added list of movies

---
+++ src/components/App/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Intro from '../Intro';
+import Series from '../../containers/Series'
+import './App.css';
+import 'whatwg-fetch';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">TV Series List</h1>
+        </header>
+        <Intro message="Here you can find your most loved series"/>
+        <Series />
+      </div>
+    );
+  }
+}
+
+export default App;
